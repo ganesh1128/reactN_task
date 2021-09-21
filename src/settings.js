9e874dca004aa6ba595956fafb635a57b30f55c5
gollavilli ganesh kumar <ganeshgollavilli@gmail.com>
2021-09-21 12:02:46 +0530

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 let getSettings = () => {
     return{
         //api : "http://localhost:3000"
-         api : "https://my-react-node-1.herokuapp.com"
+         api : "https://my-react-node-1.herokuapp.com/"
     }
 }
 
