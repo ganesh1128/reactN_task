ebb06dc7286858d578965ded4cf12767d627aa5b
gollavilli ganesh kumar <ganeshgollavilli@gmail.com>
2021-09-23 17:48:24 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import "./Login.css";
 function Home() {
     return (
         <div >
-            <h1>Welcome to ToDo Application</h1>
+            <h1>Welcome to ToDo...</h1>
              <Link to="/register">
                  <input className="btn btn-lg btn-primary ml-5 mt-2 " type="submit" value="click here for Rigester" />
                  </Link>
